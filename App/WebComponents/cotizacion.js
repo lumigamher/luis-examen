9f95051b247e1848d5cba2c1141e4329ceaf3f5f
lumigamher <lumigam@hotmail.com>
2024-03-13 09:55:37 -0500

    feat: :bug: wc cotizar

--- App/WebComponents/cotizacion.js
+++ App/WebComponents/cotizacion.js
@@ -1,13 +1,43 @@
 
-export class monetizarApp extends HTMLElement {
+export class cotizacion extends HTMLElement {
     constructor() {
       super();
       this.render();
     }
     render() {
       this.innerHTML = /* html */ `
-      
+      <div class="container" id="cotizacion">
+      <h4>¡Bien!¡Hemos Terminado!</h4>
+      <h3><Strong>Compártenos si te ha gustado</Strong></h3>
+      <div class="botones">
+          <button class="faceboton">
+              <i class='bx bxl-facebook' style='color:#ffffff'  ></i>
+              <p>Share</p>
+          </button>
+          <button class="inboton">
+              <i class='bx bxl-linkedin' style='color:#ffffff' ></i>   
+              <p>Share</p>
+          </button>
+          <button class="gboton">
+              <i class='bx bxl-google' style='color:#ffffff' ></i>
+              <p>Google + </p>
+          </button>
+          <button class="tboton">
+              <i class='bx bxl-twitter' style='color:#ffffff'></i>
+              <p>Twitt</p>
+          </button>
+      </div>
+      <h2>El costo Estimado de tu app es</h2>
+      <h1 class="tcolor1">5.000.000
+          <span>COP</span>
+      </h1>
+      <a href="" style='color:#ffffff'>Editar respuestas</a>
+      <p> En GBP contamos con los mejores <strong>desarroladores de apps y webs</strong> para tu proyecto. publica tu Proyecto en GBP.</p>
+      <div>
+          <button class="boton"><strong>Terminar</strong></button>
+      </div>
+  </div>
       `;
     }}
   
-    customElements.define('monetizar-app', monetizarApp);
+    customElements.define('cotizar-site', cotizacion);
--- js/app.js
+++ js/app.js
@@ -5,3 +5,5 @@ import '../App/WebComponents/monetizarApp.js'
 import '../App/WebComponents/diseñoApp.js'
 import '../App/WebComponents/userAutenticacion.js'
 import '../App/WebComponents/cotizacion.js'
+
+
